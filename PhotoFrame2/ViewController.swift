d94cdc218ad702426721b975b7ae8a9c5bf15ba4
swang <shouzhiwang@qq.com>
2021-09-02 17:11:53 -0400

    Initial Commit

---
+++ PhotoFrame2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame2
+//
+//  Created by 王首之 on 2021/9/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
